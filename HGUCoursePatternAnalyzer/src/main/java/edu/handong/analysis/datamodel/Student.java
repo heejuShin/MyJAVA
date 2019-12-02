35f56697dd23d05161ab2191590afcc8be4f738f
heeju Shin <21800412@handong.edu>
2019-12-02 22:50:05 +0900

    hw

---
+++ HGUCoursePatternAnalyzer/src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -0,0 +1,10 @@
+package edu.handong.analysis.datamodel;
+
+public class Student {
+	private String name;
+	public Student(String name){
+		this.name = name;
+	};
+	public String getName(){
+		return name;}
+}
